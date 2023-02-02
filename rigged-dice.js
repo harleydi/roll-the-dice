17cd35da7a12728fd8023d323200ec11afd9829c
harleydi <disneyharley26@gmail.com>
2023-02-02 10:05:09 -0500

    completed rigged dice

--- rigged-dice.js
+++ rigged-dice.js
@@ -2,15 +2,20 @@ const prompt = require('prompt-sync')({sigint: true})
 
 let rolls = 1
 
-let dice = Math.ceil(Math.random() * 6)
+// let dice = Math.ceil(Math.random() * 6)
 
-// let rigged = prompt('Please give the number you would like to be rigged: ')
-
-let riggedCount = 0
+let rigged = Number(prompt('Please give the number you would like to be rigged: '))
 
 
 
-if (rolls < 11) {
-    console.log(dice)
-    rolls++
+
+for (let i = 1; i < 7; i++) {
+    if (i % 2 === 0) {
+        // console.log(dice)
+        console.log(Math.ceil(Math.random() * 6))
+    } else {
+        console.log(rigged)
+    }
 }
+
+// console.log(dice)

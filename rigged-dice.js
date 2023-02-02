c5af50a7cfe7d42573973c12355aee56be489408
harleydi <disneyharley26@gmail.com>
2023-02-02 02:36:15 -0500

    added rigged dice, not yet completed

--- rigged-dice.js
+++ rigged-dice.js
@@ -1,5 +1,16 @@
 const prompt = require('prompt-sync')({sigint: true})
 
-let rigged = prompt('Please give the number you would like to be rigged: ')
+let rolls = 1
 
 let dice = Math.ceil(Math.random() * 6)
+
+// let rigged = prompt('Please give the number you would like to be rigged: ')
+
+let riggedCount = 0
+
+
+
+if (rolls < 11) {
+    console.log(dice)
+    rolls++
+}
